e49bfa1672e040c17ab50b9a0a184ff73385cd55
Nischal Batas <nischal.batas88@gmail.com>
2025-05-09 23:09:03 +0545

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Lora, Montserrat, Poppins } from 'next/font/google';
+import { Lora, Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 import {
   ClerkProvider,
@@ -15,23 +15,23 @@ import Link from "next/link";
 import Image from "next/image";
 import Navbar from "@/components/Index/Navbar";
 const lora = Lora({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-lora',
-  weight: ['400', '500', '600', '700'],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-lora",
+  weight: ["400", "500", "600", "700"],
 });
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-montserrat',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
 });
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -49,11 +49,7 @@ export default function RootLayout({
         <body
           className={`${lora.variable} ${montserrat.variable} ${poppins.variable} antialiased`}
         >
-        
-         
-    
-          
-   <Navbar/>
+          <Navbar />
           {children}
         </body>
       </html>
