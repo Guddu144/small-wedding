1418fa1e0f9ae30beaed67c6b2a067e4724a9447
Nischal Batas <nischal.batas88@gmail.com>
2025-05-09 23:00:56 +0545

    add

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+     <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <p className="text-2xl mb-6">Page Not Found</p>
+      <Link href="/">
+        <a className="text-blue-500 hover:underline">Go back to Home</a>
+      </Link>
+    </div>
+  )
+}
+
+export default NotFound
